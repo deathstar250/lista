a6b8f0177c81c1f032faee2e3bdcbb3ea1162e0f
Maciel Galeano <maciel.galeano@anima.edu.uy>
2019-11-12 13:04:33 -0300

    Un dia mas

---
+++ Lista_MG/Connection_PgSQL.vb
@@ -0,0 +1,18 @@
+﻿Public Class Connection_PgSQL
+    Public Function OpenConnection() As Npgsql.NpgsqlConnection
+        Dim connection As Npgsql.NpgsqlConnection
+        connection = New Npgsql.NpgsqlConnection()
+
+        Dim connectionString As String
+        connectionString = "Server=localhost;Port=4444;Database=taniveo;UserId=postgres;Password=root;"
+        Try
+            connection.ConnectionString = connectionString
+            connection.Open()
+        Catch ex As Exception
+            connection.Close()
+            Throw ex
+        End Try
+
+        Return connection
+    End Function
+End Class
---
+++ Lista_MG/Form1.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Lista
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.A1 = New System.Windows.Forms.CheckBox()
+        Me.A8 = New System.Windows.Forms.CheckBox()
+        Me.A2 = New System.Windows.Forms.CheckBox()
+        Me.A9 = New System.Windows.Forms.CheckBox()
+        Me.A3 = New System.Windows.Forms.CheckBox()
+        Me.A10 = New System.Windows.Forms.CheckBox()
+        Me.A4 = New System.Windows.Forms.CheckBox()
+        Me.A5 = New System.Windows.Forms.CheckBox()
+        Me.A6 = New System.Windows.Forms.CheckBox()
+        Me.A11 = New System.Windows.Forms.CheckBox()
+        Me.A12 = New System.Windows.Forms.CheckBox()
+        Me.A13 = New System.Windows.Forms.CheckBox()
+        Me.A7 = New System.Windows.Forms.CheckBox()
+        Me.A14 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Profesor = New System.Windows.Forms.Label()
+        Me.calendar = New System.Windows.Forms.MonthCalendar()
+        Me.aceptar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'A1
+        '
+        Me.A1.AutoSize = True
+        Me.A1.Location = New System.Drawing.Point(22, 60)
+        Me.A1.Name = "A1"
+        Me.A1.Size = New System.Drawing.Size(52, 17)
+        Me.A1.TabIndex = 0
+        Me.A1.Text = "vacio"
+        Me.A1.UseVisualStyleBackColor = True
+        '
+        'A8
+        '
+        Me.A8.AutoSize = True
+        Me.A8.Location = New System.Drawing.Point(174, 60)
+        Me.A8.Name = "A8"
+        Me.A8.Size = New System.Drawing.Size(52, 17)
+        Me.A8.TabIndex = 1
+        Me.A8.Text = "vacio"
+        Me.A8.UseVisualStyleBackColor = True
+        '
+        'A2
+        '
+        Me.A2.AutoSize = True
+        Me.A2.Location = New System.Drawing.Point(22, 83)
+        Me.A2.Name = "A2"
+        Me.A2.Size = New System.Drawing.Size(52, 17)
+        Me.A2.TabIndex = 2
+        Me.A2.Text = "vacio"
+        Me.A2.UseVisualStyleBackColor = True
+        '
+        'A9
+        '
+        Me.A9.AutoSize = True
+        Me.A9.Location = New System.Drawing.Point(174, 83)
+        Me.A9.Name = "A9"
+        Me.A9.Size = New System.Drawing.Size(52, 17)
+        Me.A9.TabIndex = 3
+        Me.A9.Text = "vacio"
+        Me.A9.UseVisualStyleBackColor = True
+        '
+        'A3
+        '
+        Me.A3.AutoSize = True
+        Me.A3.Location = New System.Drawing.Point(22, 106)
+        Me.A3.Name = "A3"
+        Me.A3.Size = New System.Drawing.Size(52, 17)
+        Me.A3.TabIndex = 4
+        Me.A3.Text = "vacio"
+        Me.A3.UseVisualStyleBackColor = True
+        '
+        'A10
+        '
+        Me.A10.AutoSize = True
+        Me.A10.Location = New System.Drawing.Point(174, 106)
+        Me.A10.Name = "A10"
+        Me.A10.Size = New System.Drawing.Size(52, 17)
+        Me.A10.TabIndex = 5
+        Me.A10.Text = "vacio"
+        Me.A10.UseVisualStyleBackColor = True
+        '
+        'A4
+        '
+        Me.A4.AutoSize = True
+        Me.A4.Location = New System.Drawing.Point(22, 129)
+        Me.A4.Name = "A4"
+        Me.A4.Size = New System.Drawing.Size(52, 17)
+        Me.A4.TabIndex = 6
+        Me.A4.Text = "vacio"
+        Me.A4.UseVisualStyleBackColor = True
+        '
+        'A5
+        '
+        Me.A5.AutoSize = True
+        Me.A5.Location = New System.Drawing.Point(22, 152)
+        Me.A5.Name = "A5"
+        Me.A5.Size = New System.Drawing.Size(52, 17)
+        Me.A5.TabIndex = 7
+        Me.A5.Text = "vacio"
+        Me.A5.UseVisualStyleBackColor = True
+        '
+        'A6
+        '
+        Me.A6.AutoSize = True
+        Me.A6.Location = New System.Drawing.Point(22, 175)
+        Me.A6.Name = "A6"
+        Me.A6.Size = New System.Drawing.Size(52, 17)
+        Me.A6.TabIndex = 8
+        Me.A6.Text = "vacio"
+        Me.A6.UseVisualStyleBackColor = True
+        '
+        'A11
+        '
+        Me.A11.AutoSize = True
+        Me.A11.Location = New System.Drawing.Point(174, 129)
+        Me.A11.Name = "A11"
+        Me.A11.Size = New System.Drawing.Size(52, 17)
+        Me.A11.TabIndex = 9
+        Me.A11.Text = "vacio"
+        Me.A11.UseVisualStyleBackColor = True
+        '
+        'A12
+        '
+        Me.A12.AutoSize = True
+        Me.A12.Location = New System.Drawing.Point(174, 152)
+        Me.A12.Name = "A12"
+        Me.A12.Size = New System.Drawing.Size(52, 17)
+        Me.A12.TabIndex = 10
+        Me.A12.Text = "vacio"
+        Me.A12.UseVisualStyleBackColor = True
+        '
+        'A13
+        '
+        Me.A13.AutoSize = True
+        Me.A13.Location = New System.Drawing.Point(174, 175)
+        Me.A13.Name = "A13"
+        Me.A13.Size = New System.Drawing.Size(52, 17)
+        Me.A13.TabIndex = 11
+        Me.A13.Text = "vacio"
+        Me.A13.UseVisualStyleBackColor = True
+        '
+        'A7
+        '
+        Me.A7.AutoSize = True
+        Me.A7.Location = New System.Drawing.Point(22, 198)
+        Me.A7.Name = "A7"
+        Me.A7.Size = New System.Drawing.Size(52, 17)
+        Me.A7.TabIndex = 12
+        Me.A7.Text = "vacio"
+        Me.A7.UseVisualStyleBackColor = True
+        '
+        'A14
+        '
+        Me.A14.AutoSize = True
+        Me.A14.Location = New System.Drawing.Point(174, 198)
+        Me.A14.Name = "A14"
+        Me.A14.Size = New System.Drawing.Size(52, 17)
+        Me.A14.TabIndex = 13
+        Me.A14.Text = "vacio"
+        Me.A14.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(94, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 13)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Lista de alumnos "
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(404, 58)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 15
+        '
+        'Profesor
+        '
+        Me.Profesor.AutoSize = True
+        Me.Profesor.Location = New System.Drawing.Point(319, 60)
+        Me.Profesor.Name = "Profesor"
+        Me.Profesor.Size = New System.Drawing.Size(46, 13)
+        Me.Profesor.TabIndex = 16
+        Me.Profesor.Text = "Profesor"
+        '
+        'calendar
+        '
+        Me.calendar.Location = New System.Drawing.Point(322, 91)
+        Me.calendar.Name = "calendar"
+        Me.calendar.TabIndex = 17
+        '
+        'aceptar
+        '
+        Me.aceptar.Location = New System.Drawing.Point(248, 309)
+        Me.aceptar.Name = "aceptar"
+        Me.aceptar.Size = New System.Drawing.Size(75, 23)
+        Me.aceptar.TabIndex = 18
+        Me.aceptar.Text = "Aceptar"
+        Me.aceptar.UseVisualStyleBackColor = True
+        '
+        'Lista
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(581, 357)
+        Me.Controls.Add(Me.aceptar)
+        Me.Controls.Add(Me.calendar)
+        Me.Controls.Add(Me.Profesor)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.A14)
+        Me.Controls.Add(Me.A7)
+        Me.Controls.Add(Me.A13)
+        Me.Controls.Add(Me.A12)
+        Me.Controls.Add(Me.A11)
+        Me.Controls.Add(Me.A6)
+        Me.Controls.Add(Me.A5)
+        Me.Controls.Add(Me.A4)
+        Me.Controls.Add(Me.A10)
+        Me.Controls.Add(Me.A3)
+        Me.Controls.Add(Me.A9)
+        Me.Controls.Add(Me.A2)
+        Me.Controls.Add(Me.A8)
+        Me.Controls.Add(Me.A1)
+        Me.Name = "Lista"
+        Me.Text = "Lista"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents A1 As CheckBox
+    Friend WithEvents A8 As CheckBox
+    Friend WithEvents A2 As CheckBox
+    Friend WithEvents A9 As CheckBox
+    Friend WithEvents A3 As CheckBox
+    Friend WithEvents A10 As CheckBox
+    Friend WithEvents A4 As CheckBox
+    Friend WithEvents A5 As CheckBox
+    Friend WithEvents A6 As CheckBox
+    Friend WithEvents A11 As CheckBox
+    Friend WithEvents A12 As CheckBox
+    Friend WithEvents A13 As CheckBox
+    Friend WithEvents A7 As CheckBox
+    Friend WithEvents A14 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Profesor As Label
+    Friend WithEvents calendar As MonthCalendar
+    Friend WithEvents aceptar As Button
+End Class
